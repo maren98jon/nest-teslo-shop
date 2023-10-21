e89fb278c04d8b91b9a71f5f7d695fdaf00d38b0
Maren <maren@develov.com>
2023-10-21 20:12:28 +0200

    Fin Seccion 12

---
+++ README.md
@@ -0,0 +1,19 @@
+<p align="center">
+  <a href="http://nestjs.com/" target="blank"><img src="https://nestjs.com/img/logo-small.svg" width="200" alt="Nest Logo" /></a>
+</p>
+
+# Teslo APPI
+
+1. Clonar proyecto
+2. ```yarn install```
+3. Cloanar el archivo ```.env.template``` y renombrarlo a ```.env```
+4. Cambiar las variables de entorno
+5. Levantar la base de datos
+```
+docker-compose up -d
+```
+6. Ejecutar Seed
+```
+http://localhost:3000/api/seed
+```
+7. Levantar dev: ```yarn start:dev```
---
+++ src/app.module.ts
@@ -0,0 +1,41 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, // en prod deberia ser false
+    }),
+
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname,'..','public'), 
+    }),
+
+    ProductsModule,
+
+    CommonModule,
+
+    SeedModule,
+
+    FilesModule,
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
